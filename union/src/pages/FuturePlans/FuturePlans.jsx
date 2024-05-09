0a44cb31c665c27c6ecf1768466d05048e6dc67a
Skybrick <97291378+Skybrick@users.noreply.github.com>
2024-05-09 07:18:53 +0200

    Create FuturePlans.jsx

---
+++ union/src/pages/FuturePlans/FuturePlans.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateFilterTransaction from "../../components/transactions/CreateFilterTransaction";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Balance Transaction System</h1>
+    </div>
+  );
+}
+
+export default App;
